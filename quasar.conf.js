c269065755d1286e271b075a72a07bc3cbac5b64
Mathew Grabau <mgrabau@mathewgrabau.ca>
2021-09-22 17:54:36 -0500

    Fixing missing reference. Still not working properly

--- quasar.conf.js
+++ quasar.conf.js
@@ -12,6 +12,8 @@ const ESLintPlugin = require('eslint-webpack-plugin');
 /* eslint global-require: 0 */
 const { configure } = require('quasar/wrappers');
 
+const path = require('path');
+
 module.exports = configure((ctx) => ({
   // https://v2.quasar.dev/quasar-cli/supporting-ts
   supportTS: false,
